4a25cde49f710adcefcb644b418a94d3d5339dbd
Joe Sak <joe@joesak.com>
2010-08-09 14:03:26 -0500

    Why verify presence and uniqueness of an attribute that doesn't exist?
    It was causing mysterious test fails, too.

--- app/models/blog_comment.rb
+++ app/models/blog_comment.rb
@@ -4,9 +4,6 @@ class BlogComment < ActiveRecord::Base
 
   acts_as_indexed :fields => [:name, :email, :body]
 
-  validates_presence_of :title
-  validates_uniqueness_of :title
-
   named_scope :approved, :conditions => {:approved => true}
   named_scope :rejected, :conditions => {:approved => false}
 end
