77546b5b65246c6af674c76ca4d0f9dcf6ce27dd
Philip Arndt <parndt@gmail.com>
2010-08-09 22:19:18 +1200

    add advanced options to blog post and prepare to add checkboxes for categories.

--- app/controllers/admin/blog/posts_controller.rb
+++ app/controllers/admin/blog/posts_controller.rb
@@ -1,5 +1,10 @@
 class Admin::Blog::PostsController < Admin::BaseController
 
   crudify :blog_post, :title_attribute => :title, :order => 'created_at DESC'
+  before_filter :find_all_categories, :only => [:new, :edit, :create, :update]
 
+protected
+  def find_all_categories
+    @blog_categories = BlogCategory.find(:all)
+  end
 end
