da6b5e1f572960533f31026981df57f2d59d1b05
Philip Arndt <parndt@gmail.com>
2010-08-26 16:00:05 +1200

    Good, brackets aren't required here.

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -16,7 +16,7 @@ class BlogPost < ActiveRecord::Base
 
   def category_ids=(ids)
     self.categories = ids.reject{|id| id.blank?}.collect {|c_id|
-      (BlogCategory.find(c_id.to_i) rescue nil)
+      BlogCategory.find(c_id.to_i) rescue nil
     }.compact
   end
 
