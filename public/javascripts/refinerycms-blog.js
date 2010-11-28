572191db89167f162f5263605c250ae60f968790
Joe Sak <joe@joesak.com>
2010-11-28 09:33:56 -0600

    This line is from an older version of the script that totally sucked

--- public/javascripts/refinerycms-blog.js
+++ public/javascripts/refinerycms-blog.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
   
   $('#next_prev_article a:not(".home")').live('click', function(){
     url = this.href + ".js";
-    nav_url = $(this).attr('data-nav-url');
     $('#show_blog_post > *').fadeOut();
     $.ajax({
       url: url,
