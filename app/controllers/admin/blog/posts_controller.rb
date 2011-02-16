a045cc11fe0291ac7e0c14bfce0d3ae12a52d56d
Joe Sak <joe@joesak.com>
2011-02-16 00:13:46 -0600

    Add controller action and view template for uncategorized posts

--- app/controllers/admin/blog/posts_controller.rb
+++ app/controllers/admin/blog/posts_controller.rb
@@ -3,6 +3,13 @@ class Admin::Blog::PostsController < Admin::BaseController
   crudify :blog_post,
           :title_attribute => :title,
           :order => 'published_at DESC'
+          
+  def uncategorized
+    @blog_posts = BlogPost.uncategorized.paginate({
+      :page => params[:page],
+      :per_page => BlogPost.per_page
+    })
+  end
 
   before_filter :find_all_categories,
                 :only => [:new, :edit, :create, :update]
