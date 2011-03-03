7ac190f9ddbd572129018f6897f28a8aad9d4401
Philip Arndt <parndt@gmail.com>
2011-03-04 11:56:38 +1300

    Moved the generic blog tables code into the migration itself.

--- db/migrate/1_create_singular_name.rb
+++ db/migrate/1_create_singular_name.rb
@@ -1,3 +1,34 @@
+@refinerycms_blog_tables = [{
+  :table_name => 'blog_posts',
+  :attributes => [
+    Rails::Generators::GeneratedAttribute.new('title', 'string'),
+    Rails::Generators::GeneratedAttribute.new('body', 'text'),
+    Rails::Generators::GeneratedAttribute.new('draft', 'boolean'),
+    Rails::Generators::GeneratedAttribute.new('published_at', 'datetime')
+  ], :id => true
+},{
+  :table_name => 'blog_comments',
+  :attributes => [
+    Rails::Generators::GeneratedAttribute.new('blog_post_id', 'integer'),
+    Rails::Generators::GeneratedAttribute.new('spam', 'boolean'),
+    Rails::Generators::GeneratedAttribute.new('name', 'string'),
+    Rails::Generators::GeneratedAttribute.new('email', 'string'),
+    Rails::Generators::GeneratedAttribute.new('body', 'text'),
+    Rails::Generators::GeneratedAttribute.new('state', 'string')
+  ], :id => true
+},{
+  :table_name => 'blog_categories',
+  :attributes => [
+    Rails::Generators::GeneratedAttribute.new('title', 'string')
+  ], :id => true
+},{
+  :table_name => 'blog_categories_blog_posts',
+  :attributes => [
+    Rails::Generators::GeneratedAttribute.new('blog_category_id', 'integer'),
+    Rails::Generators::GeneratedAttribute.new('blog_post_id', 'integer')
+  ], :id => false
+}]
+
 class Create<%= singular_name.camelize %> < ActiveRecord::Migration
 
   def self.up<% @refinerycms_blog_tables.each do |table| %>
