7746f86dbf9bfe3c8cf2eaad88d9d87858e11dbe
Joe Sak <joe@joesak.com>
2011-04-22 15:59:24 -0500

    have to explicitly require factory girl

--- features/support/factories/blog_categories.rb
+++ features/support/factories/blog_categories.rb
@@ -1,3 +1,5 @@
+require 'factory_girl'
+
 Factory.define(:blog_category) do |f|
   f.sequence(:title) { |n| "Shopping #{n}" }
 end
--- features/support/factories/blog_comments.rb
+++ features/support/factories/blog_comments.rb
@@ -1,10 +1,8 @@
-Factory.sequence :email do |n|
-  "person#{n}@example.com"
-end
+require 'factory_girl'
 
 Factory.define(:blog_comment) do |f|
   f.name "Joe Commenter"
-  f.email { Factory.next(:email) }
+  f.sequence(:email) { |n| "person#{n}@example.com" }
   f.body "Which one is the best for picking up new shoes?"
   f.association :post
 end
