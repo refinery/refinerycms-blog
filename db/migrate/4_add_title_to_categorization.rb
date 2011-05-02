d6335ba21bf72a90959d3dff9769d416d4181476
Marc Remolt <marc.remolt@googlemail.com>
2011-05-02 18:46:48 +0200

    removed unnecessary migration

--- db/migrate/4_add_title_to_categorization.rb
+++
@@ -1,9 +0,0 @@
-class AddTitleToCategorization < ActiveRecord::Migration
-  def self.up
-    add_column :blog_categories_blog_posts, :title, :string, :null => false
-  end
-
-  def self.down
-    remove_column :blog_categories_blog_posts, :title
-  end
-end
