9310b4176047dc24fce5841237441882ffee2e2f
Kyle Wilkinson <kai@wikyd.org>
2011-06-13 18:06:26 -0700

    Modify the category view of the blog to show a paged view, rather than all blog posts.

--- app/controllers/blog/categories_controller.rb
+++ app/controllers/blog/categories_controller.rb
@@ -2,6 +2,10 @@ class Blog::CategoriesController < BlogController
 
   def show
     @category = BlogCategory.find(params[:id])
+    @blog_posts = @category.posts.live.includes(:comments, :categories).paginate({
+      :page => params[:page],
+      :per_page => RefinerySetting.find_or_set(:blog_posts_per_page, 10)
+    })
   end
 
 end
