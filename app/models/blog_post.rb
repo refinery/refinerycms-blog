9320a6a608f097c8fc316b9c2a1e2a3f0bc21afb
Kyle Wilkinson <kai@wikyd.org>
2011-06-16 22:24:49 -0700

    Add logic to allow users to override the url for a post from just a title.

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -21,7 +21,7 @@ class BlogPost < ActiveRecord::Base
   validates :title, :presence => true, :uniqueness => true
   validates :body,  :presence => true
 
-  has_friendly_id :title, :use_slug => true,
+  has_friendly_id :friendly_id_source, :use_slug => true,
                   :default_locale => (::Refinery::I18n.default_frontend_locale rescue :en),
                   :approximate_ascii => RefinerySetting.find_or_set(:approximate_ascii, false, :scoping => 'blog'),
                   :strip_non_ascii => RefinerySetting.find_or_set(:strip_non_ascii, false, :scoping => 'blog')
@@ -59,6 +59,10 @@ class BlogPost < ActiveRecord::Base
     self.categories = ids.reject{|id| id.blank?}.collect {|c_id|
       BlogCategory.find(c_id.to_i) rescue nil
     }.compact
+  end
+
+  def friendly_id_source
+    custom_url.present? ? custom_url : title
   end
 
   class << self
