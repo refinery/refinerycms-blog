7028ef3dddd8559c8e5519431d3529c0c99872cc
Philip Arndt <parndt@gmail.com>
2011-07-01 09:14:46 +1200

    Making use of modules and putting classes under modules.

--- app/controllers/admin/blog/posts_controller.rb
+++ app/controllers/admin/blog/posts_controller.rb
@@ -1,86 +1,90 @@
-class Admin::Blog::PostsController < Admin::BaseController
+module Admin
+  module Blog
+    class PostsController < Admin::BaseController
 
-  crudify :blog_post,
-          :title_attribute => :title,
-          :order => 'published_at DESC'
+      crudify :blog_post,
+              :title_attribute => :title,
+              :order => 'published_at DESC'
           
-  def uncategorized
-    @blog_posts = BlogPost.uncategorized.paginate({
-      :page => params[:page],
-      :per_page => BlogPost.per_page
-    })
-  end
+      def uncategorized
+        @blog_posts = BlogPost.uncategorized.paginate({
+          :page => params[:page],
+          :per_page => BlogPost.per_page
+        })
+      end
   
-  def tags
-    op =  case ActiveRecord::Base.connection.adapter_name.downcase
-          when 'postgresql'
-            '~*'
-          else
-            'LIKE'
-          end
-    wildcard =  case ActiveRecord::Base.connection.adapter_name.downcase
-                when 'postgresql'
-                  '.*'
-                else
-                  '%'
-                end
-    @tags = BlogPost.tag_counts_on(:tags).where(
-        ["tags.name #{op} ?", "#{wildcard}#{params[:term].to_s.downcase}#{wildcard}"]
-      ).map { |tag| {:id => tag.id, :value => tag.name}}
-    render :json => @tags.flatten
-  end
+      def tags
+        op =  case ActiveRecord::Base.connection.adapter_name.downcase
+              when 'postgresql'
+                '~*'
+              else
+                'LIKE'
+              end
+        wildcard =  case ActiveRecord::Base.connection.adapter_name.downcase
+                    when 'postgresql'
+                      '.*'
+                    else
+                      '%'
+                    end
+        @tags = BlogPost.tag_counts_on(:tags).where(
+            ["tags.name #{op} ?", "#{wildcard}#{params[:term].to_s.downcase}#{wildcard}"]
+          ).map { |tag| {:id => tag.id, :value => tag.name}}
+        render :json => @tags.flatten
+      end
   
-  def create
-    # if the position field exists, set this object as last object, given the conditions of this class.
-    if BlogPost.column_names.include?("position")
-      params[:blog_post].merge!({
-        :position => ((BlogPost.maximum(:position, :conditions => "")||-1) + 1)
-      })
-    end
+      def create
+        # if the position field exists, set this object as last object, given the conditions of this class.
+        if BlogPost.column_names.include?("position")
+          params[:blog_post].merge!({
+            :position => ((BlogPost.maximum(:position, :conditions => "")||-1) + 1)
+          })
+        end
     
-    if BlogPost.column_names.include?("user_id")
-      params[:blog_post].merge!({
-        :user_id => current_user.id
-      })
-    end
+        if BlogPost.column_names.include?("user_id")
+          params[:blog_post].merge!({
+            :user_id => current_user.id
+          })
+        end
 
-    if (@blog_post = BlogPost.create(params[:blog_post])).valid?
-      (request.xhr? ? flash.now : flash).notice = t(
-        'refinery.crudify.created',
-        :what => "'#{@blog_post.title}'"
-      )
+        if (@blog_post = BlogPost.create(params[:blog_post])).valid?
+          (request.xhr? ? flash.now : flash).notice = t(
+            'refinery.crudify.created',
+            :what => "'#{@blog_post.title}'"
+          )
 
-      unless from_dialog?
-        unless params[:continue_editing] =~ /true|on|1/
-          redirect_back_or_default(admin_blog_posts_url)
+          unless from_dialog?
+            unless params[:continue_editing] =~ /true|on|1/
+              redirect_back_or_default(admin_blog_posts_url)
+            else
+              unless request.xhr?
+                redirect_to :back
+              else
+                render :partial => "/shared/message"
+              end
+            end
+          else
+            render :text => "<script>parent.window.location = '#{admin_blog_posts_url}';</script>"
+          end
         else
           unless request.xhr?
-            redirect_to :back
+            render :action => 'new'
           else
-            render :partial => "/shared/message"
+            render :partial => "/shared/admin/error_messages",
+                   :locals => {
+                     :object => @blog_post,
+                     :include_object_name => true
+                   }
           end
         end
-      else
-        render :text => "<script>parent.window.location = '#{admin_blog_posts_url}';</script>"
       end
-    else
-      unless request.xhr?
-        render :action => 'new'
-      else
-        render :partial => "/shared/admin/error_messages",
-               :locals => {
-                 :object => @blog_post,
-                 :include_object_name => true
-               }
+
+      before_filter :find_all_categories,
+                    :only => [:new, :edit, :create, :update]
+
+    protected
+      def find_all_categories
+        @blog_categories = BlogCategory.find(:all)
       end
     end
-  end
-
-  before_filter :find_all_categories,
-                :only => [:new, :edit, :create, :update]
-
-protected
-  def find_all_categories
-    @blog_categories = BlogCategory.find(:all)
   end
 end
