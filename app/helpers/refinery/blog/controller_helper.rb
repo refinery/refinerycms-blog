362088d0272d6d61824b0539e201356cf9219dc0
Jamie Winsor <jamie@enmasse.com>
2011-09-19 18:50:38 -0700

    Fix indent on protected methods

--- app/helpers/refinery/blog/controller_helper.rb
+++ app/helpers/refinery/blog/controller_helper.rb
@@ -4,27 +4,27 @@ module Refinery
     
       protected
     
-        def find_blog_post
-          unless (@blog_post = Refinery::BlogPost.find(params[:id])).try(:live?)
-            if refinery_user? and current_user.authorized_plugins.include?("refinerycms_blog")
-              @blog_post = Refinery::BlogPost.find(params[:id])
-            else
-              error_404
-            end
+      def find_blog_post
+        unless (@blog_post = Refinery::BlogPost.find(params[:id])).try(:live?)
+          if refinery_user? and current_user.authorized_plugins.include?("refinerycms_blog")
+            @blog_post = Refinery::BlogPost.find(params[:id])
+          else
+            error_404
           end
         end
-    
-        def find_all_blog_posts
-          @blog_posts = Refinery::BlogPost.live.includes(:comments, :categories).page(params[:page])
-        end
+      end
+  
+      def find_all_blog_posts
+        @blog_posts = Refinery::BlogPost.live.includes(:comments, :categories).page(params[:page])
+      end
 
-        def find_tags
-          @tags = Refinery::BlogPost.tag_counts_on(:tags)
-        end
-      
-        def find_all_blog_categories
-          @blog_categories = Refinery::BlogCategory.all
-        end
+      def find_tags
+        @tags = Refinery::BlogPost.tag_counts_on(:tags)
+      end
+    
+      def find_all_blog_categories
+        @blog_categories = Refinery::BlogCategory.all
+      end
     end
   end
 end
