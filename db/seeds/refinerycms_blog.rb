a0ec1152729573dc825f41df97eefa222b83c251
Jeff Shumate <jeff@catapulsion.net>
2011-12-28 23:10:14 -0700

    fix seeds to use correct config

--- db/seeds/refinerycms_blog.rb
+++ db/seeds/refinerycms_blog.rb
@@ -14,7 +14,7 @@ if defined?(::Page)
     :menu_match => "^/blogs?(\/|\/.+?|)$"
   )
 
-  ::Page.default_parts.each do |default_page_part|
+  Refinery::Pages.config.default_parts.each do |default_page_part|
     page.parts.create(:title => default_page_part, :body => nil)
   end
 end
