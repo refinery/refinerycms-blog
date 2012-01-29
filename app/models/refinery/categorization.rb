44a059a47502eba436de440b542be76807501953
Philip Arndt <parndt@gmail.com>
2012-01-30 11:48:24 +1300

    Fix for deprecation in Rails 3.2

--- app/models/refinery/categorization.rb
+++ app/models/refinery/categorization.rb
@@ -1,7 +1,7 @@
 module Refinery
   class Categorization < ActiveRecord::Base
 
-    set_table_name 'refinery_blog_categories_blog_posts'
+    self.table_name = 'refinery_blog_categories_blog_posts'
     belongs_to :blog_post, :class_name => 'Refinery::Blog::Post', :foreign_key => :blog_post_id
     belongs_to :blog_category, :class_name => 'Refinery::Blog::Category', :foreign_key => :blog_category_id
 
