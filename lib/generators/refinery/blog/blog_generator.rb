ba9091cd14e7179e702a6ba1f3a1957a6691be0b
Uģis Ozols <ugis.ozolss@gmail.com>
2012-02-02 15:49:07 +0200

    Make sure there is seeds.rb file before trying to append to it.

--- lib/generators/refinery/blog/blog_generator.rb
+++ lib/generators/refinery/blog/blog_generator.rb
@@ -6,6 +6,7 @@ module Refinery
     end
 
     def append_load_seed_data
+      create_file 'db/seeds.rb' unless File.exists?(File.join(destination_root, 'db', 'seeds.rb'))
       append_file 'db/seeds.rb', :verbose => true do
         <<-EOH
 
