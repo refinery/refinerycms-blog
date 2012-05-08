9957fb270c216fc373cebde1fd6e9e34aafaf5f2
Carlos Betancourt C. <carbetacar@gmail.com>
2012-05-08 10:06:08 -0530

    Don't print the draft post whitin popular posts.

--- app/models/refinery/blog/post.rb
+++ app/models/refinery/blog/post.rb
@@ -70,7 +70,7 @@ module Refinery
         end
 
         def popular(count)
-          unscoped.order("access_count DESC").limit(count)
+          unscoped.order("access_count DESC").where(:draft => :false).limit(count)
         end
 
         def previous(item)
