f8873becb030843f58d30b237900ebb8a861ef60
Adrien Coquio <adrien.coquio@gmail.com>
2012-06-02 10:21:10 +0200

    Added migrate_data option on migrations for translations

--- db/migrate/20120530102901_create_blog_translations.rb
+++ db/migrate/20120530102901_create_blog_translations.rb
@@ -6,10 +6,11 @@ class CreateBlogTranslations < ActiveRecord::Migration
                                                      custom_url: :string,
                                                      slug: :string,
                                                      title: :string,
-                                                   })
+                                                   },
+                                                   { :migrate_data => true})
   end
 
   def down
-    Refinery::Blog::Post.drop_translation_table!    
+    Refinery::Blog::Post.drop_translation_table! :migrate_data => true
   end
 end
