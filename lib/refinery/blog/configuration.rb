6cc795274d9d1de415951dd6d8e1c0d1c99e237c
John Agan <johnagan@gmail.com>
2012-07-31 00:07:49 -0400

    Added the ability to use a different url than "/blog"

--- lib/refinery/blog/configuration.rb
+++ lib/refinery/blog/configuration.rb
@@ -3,12 +3,13 @@ module Refinery
     include ActiveSupport::Configurable
 
     config_accessor :validate_source_url, :comments_per_page, :posts_per_page,
-      :post_teaser_length, :share_this_key
+      :post_teaser_length, :share_this_key, :page_url
 
     self.validate_source_url = false
     self.comments_per_page = 10
     self.posts_per_page = 10
     self.post_teaser_length = 250
     self.share_this_key = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    self.page_url = "/blog"
   end
 end
