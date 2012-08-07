6cdcc6fd42f3e99ae86ab73a9bbd6dd0055a0dfb
Brice Sanchez <brice.sanchez@nurun.com>
2012-08-07 14:42:42 -0400

    Fix for model user

--- app/models/refinery/blog/user.rb
+++ app/models/refinery/blog/user.rb
@@ -1,9 +1,6 @@
 module Refinery
-  module Blog
-    class User < ActiveRecord::Base
-      extend FriendlyId
-      friendly_id :username, :use => [:slugged]
-
-    end
+  class User < ActiveRecord::Base
+    extend FriendlyId
+    friendly_id :username, :use => [:slugged]
   end
 end
