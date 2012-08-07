d91693f77b62e121100553153e2531e71c14fe86
Brice Sanchez <brice.sanchez@nurun.com>
2012-08-07 14:01:44 -0400

    percaline test

--- app/controllers/refinery/blog/authors_controller.rb
+++ app/controllers/refinery/blog/authors_controller.rb
@@ -3,7 +3,7 @@ module Refinery
     class AuthorsController < BlogController
 
       def show
-        @author = Refinery::User.find(params[:id])
+        @author = Refinery::User.find_by_permalink(params[:id])
         @posts = Refinery::Blog::Post.where(:user_id => @author.id).live.includes(:comments, :categories).page(params[:page])
       end
 
