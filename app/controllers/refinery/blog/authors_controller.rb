95a5ffa2ae691e5f22755765349f9951adfa77ff
Brice Sanchez <brice.sanchez@nurun.com>
2012-08-07 12:21:58 -0400

    Fix for method Users

--- app/controllers/refinery/blog/authors_controller.rb
+++ app/controllers/refinery/blog/authors_controller.rb
@@ -3,7 +3,7 @@ module Refinery
     class AuthorsController < BlogController
 
       def show
-        @author = Refinery::User.find(params[:id])
+        @author = Refinery::Users.find(params[:id])
         @posts = @author.posts.live.includes(:comments, :categories).page(params[:page])
       end
 
