a1bdf07e3484b7f0d688ed541c49277828a24973
Brice Sanchez <brice.sanchez@nurun.com>
2012-08-07 14:21:52 -0400

    Test of improvement url

--- app/controllers/refinery/blog/authors_controller.rb
+++ app/controllers/refinery/blog/authors_controller.rb
@@ -3,7 +3,7 @@ module Refinery
     class AuthorsController < BlogController
 
       def show
-        @author = Refinery::User.find(params[:id])
+        @author = Refinery::User.find(params[:user])
         @posts = Refinery::Blog::Post.where(:user_id => @author.id).live.includes(:comments, :categories).page(params[:page])
       end
 
