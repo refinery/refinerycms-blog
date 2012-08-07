dc1b4e1a78b83371805e2d144c145def9c0aa711
Brice Sanchez <brice.sanchez@nurun.com>
2012-08-07 14:45:33 -0400

    Fix for find user by id

--- app/controllers/refinery/blog/authors_controller.rb
+++ app/controllers/refinery/blog/authors_controller.rb
@@ -3,7 +3,7 @@ module Refinery
     class AuthorsController < BlogController
 
       def show
-        @author = Refinery::User.find(params[:user])
+        @author = Refinery::User.find(params[:id])
         @posts = Refinery::Blog::Post.where(:user_id => @author.id).live.includes(:comments, :categories).page(params[:page])
       end
 
