2ac9400cc5d91315a9d51e26577e759a7d2597f1
Brice Sanchez <brice.sanchez@nurun.com>
2012-08-08 09:31:42 -0400

    Fix for association named posts bug

--- app/controllers/refinery/blog/authors_controller.rb
+++ app/controllers/refinery/blog/authors_controller.rb
@@ -4,7 +4,7 @@ module Refinery
 
       def show
         @author = Refinery::User.find(params[:id])
-        @posts = @author.posts.live.includes(:comments, :categories).page(params[:page])
+        @posts = @author.posts.live.includes(:posts, :comments, :categories).page(params[:page])
       end
 
     end
