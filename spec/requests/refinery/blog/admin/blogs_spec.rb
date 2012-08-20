ca94c4d12aa3dd6deb8b503000bcd779dc368671
Rodrigo García Suárez <rodrigo@simplelogica.net>
2012-08-20 11:29:22 +0200

    Added blog controller for admin section. With tests.

---
+++ spec/requests/refinery/blog/admin/blogs_spec.rb
@@ -0,0 +1,100 @@
+# encoding: utf-8
+require "spec_helper"
+
+describe Refinery do
+  describe "Blog" do
+    describe "Admin" do
+      describe "blogs" do
+        refinery_login_with :refinery_user
+
+        describe "blogs list" do
+          before do
+            FactoryGirl.create(:blog, :name => "UniqueTitleOne")
+            FactoryGirl.create(:blog, :name => "UniqueTitleTwo")
+          end
+
+          it "shows two items" do
+            visit refinery.blog_admin_blogs_path
+            page.should have_content("UniqueTitleOne")
+            page.should have_content("UniqueTitleTwo")
+          end
+        end
+
+        describe "create" do
+          before do
+            visit refinery.blog_admin_blogs_path
+            click_link "Add New Blog"
+          end
+
+          context "valid data" do
+            it "should succeed" do
+              fill_in "Name", :with => "This is a test of the first string field"
+              click_button "Save"
+
+              page.should have_content("'This is a test of the first string field' was successfully added.")
+              Refinery::Blog::Blog.count.should == 1
+            end
+          end
+
+          context "invalid data" do
+            it "should fail" do
+              click_button "Save"
+
+              page.should have_content("Name can't be blank")
+              Refinery::Blog::Blog.count.should == 0
+            end
+          end
+
+          context "duplicate" do
+            before { FactoryGirl.create(:blog, :name => "UniqueTitle") }
+
+            it "should fail" do
+              visit refinery.blog_admin_blogs_path
+
+              click_link "Add New Blog"
+
+              fill_in "Name", :with => "UniqueTitle"
+              click_button "Save"
+
+              page.should have_content("There were problems")
+              Refinery::Blog::Blog.count.should == 1
+            end
+          end
+
+        end
+
+        describe "edit" do
+          before { FactoryGirl.create(:blog, :name => "A name") }
+
+          it "should succeed" do
+            visit refinery.blog_admin_blogs_path
+
+            within ".actions" do
+              click_link "Edit this blog"
+            end
+
+            fill_in "Name", :with => "A different name"
+            click_button "Save"
+
+            page.should have_content("'A different name' was successfully updated.")
+            page.should have_no_content("A name")
+          end
+        end
+
+        describe "destroy" do
+          before { FactoryGirl.create(:blog, :name => "UniqueTitleOne") }
+
+          it "should succeed" do
+            visit refinery.blog_admin_blogs_path
+
+            click_link "Remove this blog forever"
+
+            page.should have_content("'UniqueTitleOne' was successfully removed.")
+            Refinery::Blog::Blog.count.should == 0
+          end
+        end
+
+      end
+    end
+  end
+end
