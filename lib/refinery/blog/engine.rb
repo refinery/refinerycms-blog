6c9052b3be993a99f37dd029324bba5bd2e80d5b
Rodrigo García Suárez <rodrigo@simplelogica.net>
2012-08-21 10:31:36 +0200

    - Beginning refactorization in routes of posts, comments and   categories as nested models of blog.
    - Changed submenu partial so it now expects the blog to be passed as a
      local param.
    
    - Fixed errata in tests.

--- lib/refinery/blog/engine.rb
+++ lib/refinery/blog/engine.rb
@@ -9,7 +9,7 @@ module Refinery
         Refinery::Plugin.register do |plugin|
           plugin.pathname = root
           plugin.name = "refinerycms_blog"
-          plugin.url = proc { Refinery::Core::Engine.routes.url_helpers.blog_admin_posts_path }
+          plugin.url = proc { Refinery::Core::Engine.routes.url_helpers.blog_admin_blogs_path }
           plugin.menu_match = /refinery\/blog\/?(blogs|posts|comments|categories)?/
           plugin.activity = { :class_name => :'refinery/blog/post' }
         end
