bd2e18c7eff5c84613b56ac29812bfe771ca4f32
Rodrigo García Suárez <rodrigo@simplelogica.net>
2012-08-22 12:19:10 +0200

    Updated comments controller tests.

--- spec/controllers/refinery/blog/admin/comments_controller_spec.rb
+++ spec/controllers/refinery/blog/admin/comments_controller_spec.rb
@@ -6,75 +6,83 @@ module Refinery
       describe CommentsController do
         refinery_login_with :refinery_user
 
+        let! (:blog) { FactoryGirl.create(:blog) }
+        let! (:_post) { FactoryGirl.create(:blog_post, :blog => blog) }
+        
         describe "#index" do
-          let!(:comment) { FactoryGirl.create(:blog_comment) }
+          let!(:comment) { FactoryGirl.create(:blog_comment,
+                                              :post => _post) }
           
           it "succeeds" do
-            get :index
+            get :index, :blog_id => blog.id
             response.should be_success
             response.should render_template(:index)
           end
 
           it "assigns unmoderated comments" do
-            get :index
+            get :index, :blog_id => blog.id
             assigns(:comments).first.should eq(comment)
           end
         end
 
         describe "#approved" do
-          let!(:comment) { FactoryGirl.create(:approved_comment) }
+          let!(:comment) { FactoryGirl.create(:approved_comment,
+                                              :post => _post) }
 
           it "succeeds" do
-            get :approved
+            get :approved, :blog_id => blog.id
             response.should be_success
             response.should render_template(:index)
           end
 
           it "assigns approved comments" do
-            get :approved
+            get :approved, :blog_id => blog.id
             assigns(:comments).first.should eq(comment)
           end
         end
 
         describe "#approve" do
-          let!(:comment) { FactoryGirl.create(:blog_comment) }
+          let!(:comment) { FactoryGirl.create(:blog_comment,
+                                              :post => _post) }
 
-          it "redirects on success" do
-            post :approve, :id => comment.id
+           it "redirects on success" do
+            post :approve, :blog_id => blog.id, :id => comment.id
             response.should be_redirect
           end
           
           it "approves the comment" do
-            post :approve, :id => comment.id
+            post :approve, :blog_id => blog.id, :id => comment.id
             Refinery::Blog::Comment.approved.count.should eq(1)
           end
         end
         
         describe "#rejected" do
-          let!(:comment) { FactoryGirl.create(:rejected_comment) }
+          let!(:comment) { FactoryGirl.create(:rejected_comment,
+                                              :post => _post) }
 
           it "succeeds" do
-            get :rejected
+            get :rejected, :blog_id => blog.id
             response.should be_success
             response.should render_template(:index)
           end
 
           it "assigns rejected comments" do
-            get :rejected
+            get :rejected, :blog_id => blog.id
             assigns(:comments).first.should eq(comment)
           end
         end
 
         describe "#reject" do
-          let!(:comment) { FactoryGirl.create(:blog_comment) }
+          let!(:comment) { FactoryGirl.create(:blog_comment,
+                                              :post => _post) }
 
           it "redirects on success" do
-            post :reject, :id => comment.id
+            post :reject, :blog_id => blog.id, :id => comment.id
             response.should be_redirect
           end
           
           it "rejects the comment" do
-            post :reject, :id => comment.id
+            post :reject, :blog_id => blog.id, :id => comment.id
             Refinery::Blog::Comment.rejected.count.should eq(1)
           end
         end
