f84c486e28e66a72aa1dd173449e76982cf08707
Rodrigo García Suárez <rodrigo@simplelogica.net>
2012-09-06 13:22:22 +0200

    - Added code responsable of saving for all the locales the slug of the   blogs page in the seeds file.
    - Changed indentation (for a more conventional one).

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 Refinery::User.all.each do |user|
   if user.plugins.where(:name => 'refinerycms_blog').blank?
     user.plugins.create(:name => "refinerycms_blog",
-                        :position => (user.plugins.maximum(:position) || -1) +1)
+      :position => (user.plugins.maximum(:position) || -1) +1)
   end
 end if defined?(Refinery::User)
 
@@ -11,9 +11,14 @@ if defined?(Refinery::Page) and !Refinery::Page.exists?(:link_url => '/blogs')
     :link_url => "/blogs",
     :deletable => false,
     :menu_match => "^/blogs?(\/|\/.+?|)$"
-  )
+    )
 
   Refinery::Pages.default_parts.each do |default_page_part|
     page.parts.create(:title => default_page_part, :body => nil)
+  end
+
+  (Refinery.i18n_enabled? ? Refinery::I18n.frontend_locales : [:en]).each do |lang|
+    I18n.locale = lang
+    Refinery::Page.find_by_title("Blogs").update_attributes(:slug => "blogs")
   end
 end
