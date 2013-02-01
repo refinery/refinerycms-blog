636383e630967c373d762ae2475f4f4e7e05f974
Jonathan Seitel <jon.seitel@gmail.com>
2013-01-31 22:23:49 -0800

    Committing user_class to master

--- app/models/refinery/blog/post.rb
+++ app/models/refinery/blog/post.rb
@@ -14,7 +14,7 @@ module Refinery
 
       default_scope :order => 'published_at DESC'
 
-      belongs_to :author, :class_name => 'Refinery::User', :foreign_key => :user_id, :readonly => true
+      belongs_to :author, :class_name => Refinery::Blog.user_class.to_s, :foreign_key => :user_id, :readonly => true
 
       has_many :comments, :dependent => :destroy, :foreign_key => :blog_post_id
       acts_as_taggable
