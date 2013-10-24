2e011644a8da579d2fa22187cf0e6187cf13ec65
Jérémy FRERE <frere.jeremy@gmail.com>
2013-10-24 16:28:39 +0200

    Adds spec to check no other translation had been deleted

--- spec/controllers/refinery/blog/admin/posts_controller_spec.rb
+++ spec/controllers/refinery/blog/admin/posts_controller_spec.rb
@@ -11,11 +11,17 @@ module Refinery
 
           before do
             blog_post.translations.create(:locale => :fr, :title => 'Un titre francais', :body => "La baguette, c'est bon. Mangez-en.")
+            blog_post.translations.create(:locale => :es, :title => 'Un titulo espanol', :body => "Mi casa e su casa.")
           end
 
           it "destroys the translation" do
             post :delete_translation, :id => blog_post.id, :locale_to_delete => :fr
             blog_post.translations.exists?(:locale => :fr).should be_false
+          end
+
+          it "does not destroy other translations" do
+            post :delete_translation, :id => blog_post.id, :locale_to_delete => :fr
+            blog_post.translations.exists?(:locale => :es).should be_true
           end
 
           it "redirects on success" do
