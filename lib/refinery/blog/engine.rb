3e9b604d1bdc9c3f1475a35c658d425a85d4a8c3
Philip Arndt <git@arndt.io>
2014-11-12 22:10:50 -0600

    Give the test user access to refinerycms_blog extension.
    Fixes #392

--- lib/refinery/blog/engine.rb
+++ lib/refinery/blog/engine.rb
@@ -10,7 +10,7 @@ module Refinery
           plugin.pathname = root
           plugin.name = "refinerycms_blog"
           plugin.url = proc { Refinery::Core::Engine.routes.url_helpers.blog_admin_posts_path }
-          plugin.menu_match = /refinery\/blog\/?(posts|comments|categories)?/
+          plugin.menu_match = %r{refinery/blog/?(posts|comments|categories)?}
         end
       end
 
