cc5c2b7d53e58ec5e6f47693da05765f13109105
Anita Graham <anita@joli.com.au>
2015-05-07 22:30:57 +0800

    Use find/trigger(:click) instead of click_link

--- spec/features/refinery/blog/admin/posts_spec.rb
+++ spec/features/refinery/blog/admin/posts_spec.rb
@@ -33,7 +33,7 @@ module Refinery
             end
 
             it "should have category title", :js => true do
-              click_link "toggle_advanced_options"
+              find('a', text: 'Toggle Advanced Options').trigger(:click)
               expect(page).to have_content(blog_category.title)
             end
 
@@ -45,7 +45,7 @@ module Refinery
                 # this is a dirty hack but textarea that needs to be filled is
                 # hidden and capybara refuses to fill in elements it can't see
                 page.evaluate_script("WYMeditor.INSTANCES[0].html('<p>And I love it</p>')")
-                click_link "toggle_advanced_options"
+                find('a', text: 'Toggle Advanced Options').trigger(:click)
                 expect(page).to have_css '.blog_categories'
                 expect(page).to have_css "#post_category_ids_#{blog_category.id}"
                 check blog_category.title
@@ -168,7 +168,8 @@ module Refinery
               # hidden and capybara refuses to fill in elements it can't see
               page.evaluate_script("WYMeditor.INSTANCES[0].html('<p>I totally did not write it.</p>')")
 
-              click_link "toggle_advanced_options"
+              find('a', text: 'Toggle Advanced Options').trigger(:click)
+
 
               select other_guy.username, :from => "Author"
 
