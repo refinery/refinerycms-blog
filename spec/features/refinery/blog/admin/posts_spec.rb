7b92b952616f3fe558747e253b9f85e7891c8aa0
Anita Graham <anita@joli.com.au>
2015-05-13 13:17:12 +0800

    Use ids to find, click to click.

--- spec/features/refinery/blog/admin/posts_spec.rb
+++ spec/features/refinery/blog/admin/posts_spec.rb
@@ -33,7 +33,7 @@ module Refinery
             end
 
             it "should have category title", :js => true do
-              find('a', text: 'Toggle Advanced Options').trigger(:click)
+              find('#toggle_advanced_options').click
               expect(page).to have_content(blog_category.title)
             end
 
@@ -45,7 +45,7 @@ module Refinery
                 # this is a dirty hack but textarea that needs to be filled is
                 # hidden and capybara refuses to fill in elements it can't see
                 page.evaluate_script("WYMeditor.INSTANCES[0].html('<p>And I love it</p>')")
-                find('a', text: 'Toggle Advanced Options').trigger(:click)
+                find('#toggle_advanced_options').click
                 expect(page).to have_css '.blog_categories'
                 expect(page).to have_css "#post_category_ids_#{blog_category.id}"
                 expect(page).to have_selector("#post_category_ids_#{blog_category.id}:not(:checked)")
