da43460e82382d4887490fbf3fe7ecd79442072d
Philip Arndt <git@p.arndt.io>
2015-08-10 12:09:43 +1200

    Attempt to remove coupling to Refinery::User
    This will help with #446

--- lib/refinery/blog/configuration.rb
+++ lib/refinery/blog/configuration.rb
@@ -11,27 +11,14 @@ module Refinery
     self.post_teaser_length = 250
     self.share_this_key = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
     self.page_url = "/blog"
-  
-    # Refinery::User isn't available when this line gets hit, so we use static methods instead
-    @@user_class_name = nil
+
     class << self
       def user_class=(class_name)
-        if class_name.is_a?(Class)
-          raise TypeError, "You can't set user_class to be a class, e.g., User.  Instead, please use a string like 'User'"
-        elsif class_name.is_a?(String)
-          @@user_class_name = class_name
-        else
-          raise TypeError, "Invalid type for user_class.  Please use a string like 'User'"
-        end
+        raise NotImplementedError, "Refinery::Blog#user_class= has been removed."
       end
 
       def user_class
-        class_name = @@user_class_name || "Refinery::Authentication::Devise::User"
-        begin
-          Object.const_get(class_name)
-        rescue NameError
-          class_name.constantize
-        end
+        raise NotImplementedError, "Refinery::Blog#user_class has been removed."
       end
     end
   end
