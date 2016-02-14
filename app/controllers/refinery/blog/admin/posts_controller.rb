8d052c50e20da74a17b284225f69b5d4c1beb52e
Philip Arndt <git@p.arndt.io>
2016-02-14 16:01:05 +1300

    Added permitted_post_params to PostsController
    In the style of refinerycms core extensions

--- app/controllers/refinery/blog/admin/posts_controller.rb
+++ app/controllers/refinery/blog/admin/posts_controller.rb
@@ -85,9 +85,15 @@ module Refinery
       private
 
         def post_params
-          params.require(:post).permit(:title, :body, :custom_teaser, :tag_list,
+          params.require(:post).permit(permitted_post_params)
+        end
+        
+        def permitted_post_params
+          [
+            :title, :body, :custom_teaser, :tag_list,
             :draft, :published_at, :custom_url, :user_id, :browser_title,
-            :meta_description, :source_url, :source_url_title, :category_ids => [])
+            :meta_description, :source_url, :source_url_title, :category_ids => []
+          ]
         end
 
       protected
