d03c2a0bbdf806fd8b9a3546ed01455a1e0a92fa
Brice Sanchez <contact@brice-sanchez.com>
2016-11-05 23:37:06 -0400

    Update syntax

--- app/controllers/refinery/blog/blog_controller.rb
+++ app/controllers/refinery/blog/blog_controller.rb
@@ -5,12 +5,12 @@ module Refinery
       include ControllerHelper
 
       helper :'refinery/blog/posts'
-      before_filter :find_page, :find_all_blog_categories
+      before_action :find_page, :find_all_blog_categories
 
       protected
 
         def find_page
-          @page = Refinery::Page.find_by(:link_url => Refinery::Blog.page_url)
+          @page = Refinery::Page.find_by(link_url: Refinery::Blog.page_url)
         end
     end
   end
