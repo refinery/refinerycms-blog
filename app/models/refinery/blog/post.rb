636a0fa7d969a2db48d30c2f1424f6aa567f1325
Brice Sanchez <bricesanchez@users.noreply.github.com>
2018-02-10 23:35:41 -0500

    belongs_to :author in Blog::Post is now optional
    explicitly needed since Rails 5.1

--- app/models/refinery/blog/post.rb
+++ app/models/refinery/blog/post.rb
@@ -33,7 +33,8 @@ module Refinery
 
       acts_as_taggable
 
-      belongs_to :author, proc {readonly(true)}, :class_name => Refinery::Blog.user_class.to_s, :foreign_key => :user_id
+      belongs_to :author, proc { readonly(true) }, class_name: Refinery::Blog.user_class.to_s, foreign_key: :user_id, optional: true
+
       has_many :comments, :dependent => :destroy, :foreign_key => :blog_post_id
       has_many :categorizations, :dependent => :destroy, :foreign_key => :blog_post_id, inverse_of: :blog_post
       has_many :categories, :through => :categorizations, :source => :blog_category
