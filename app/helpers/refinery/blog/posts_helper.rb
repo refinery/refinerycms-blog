00970c2bae6bccf453ab61c44a6a743d561c9982
Philip Arndt <git@p.arndt.io>
2019-03-15 16:24:11 +1300

    Fix indentation
    For some reason this was out by 1 space.

--- app/helpers/refinery/blog/posts_helper.rb
+++ app/helpers/refinery/blog/posts_helper.rb
@@ -11,11 +11,11 @@ module Refinery
 
       def blog_post_teaser(post)
         if post.respond_to?(:custom_teaser) && post.custom_teaser.present?
-         post.custom_teaser.html_safe
+          post.custom_teaser.html_safe
         else
-         truncate(post.body, {
-           :length => Refinery::Blog.post_teaser_length,
-           :preserve_html_tags => true
+          truncate(post.body, {
+            :length => Refinery::Blog.post_teaser_length,
+            :preserve_html_tags => true
           }).html_safe
         end
       end
