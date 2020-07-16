953d1ba7a0b0516feaa2c734e167196188602257
Stéphane Hanser <shanser@captive.fr>
2020-07-16 16:08:05 +0200

    Upgrade Syntax so it does not display any warning at test execution
    Co-authored-by: Marine Sourin <msourin@captive.fr>

--- spec/factories/blog_posts.rb
+++ spec/factories/blog_posts.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
-  factory :blog_post, :class => Refinery::Blog::Post do
+  factory :blog_post, class: Refinery::Blog::Post do
     sequence(:title) { |n| "Top #{n} Shopping Centers in Chicago" }
     body { "These are the top ten shopping centers in Chicago. You're going to read a long blog post about them. Come to peace with it." }
     draft { false }
     published_at { Time.now }
-    username { "John Doe" }
+    username { 'John Doe' }
 
     factory :blog_post_draft do
       draft { true }
