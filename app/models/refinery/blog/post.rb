51a84a564ad8ad4a0150eb9170d78227ae4c1d9c
Anita <anita@joli.com.au>
2023-05-23 13:36:50 +0800

    Removed FriendlyId options in post.rb Added Mobility, removed  Globalize, updated responders

--- app/models/refinery/blog/post.rb
+++ app/models/refinery/blog/post.rb
@@ -15,21 +15,6 @@ module Refinery
         is_seo_meta
       end
 
-      class FriendlyIdOptions
-        def self.options
-          # Docs for friendly_id https://github.com/norman/friendly_id
-          friendly_id_options = {
-            use: [:mobility, :reserved],
-            reserved_words: Refinery::Pages.friendly_id_reserved_words
-          }
-          if ::Refinery::Blog.scope_slug_by_parent
-            friendly_id_options[:use] << :scoped
-            friendly_id_options.merge!(scope: :parent)
-          end
-
-          friendly_id_options
-        end
-      end
       extend FriendlyId
       friendly_id :friendly_id_source, use:  [:mobility, :slugged]
 
